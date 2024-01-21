9780d46bf294c615aa8a57c2f5a05283a207c966
Desmond <desmondmpofu@gmail.com>
2024-01-21 13:53:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logoImg from './assets/react-core-concepts.png';
+const reactDescriptions= ['Fundamental', 'Crucial', 'Core' ]
+function getRandomInt(max){
+    return Math.floor(Math.random()*(max+1));
+}
+
+function Header(){
+    return(
+        <header>
+        <img src={logoImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    )
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
